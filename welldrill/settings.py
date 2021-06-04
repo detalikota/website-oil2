e3d8a5d61f0e5a93ff5dd77cda5eb60e4706278a
detalikota <detalikota@gmail.com>
2021-06-04 14:22:34 +0300

    hided secret key

--- welldrill/settings.py
+++ welldrill/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cczrbxx)7unpkuc3^vu4e=-)dkmt3^d^m8q=1#nb!g7n5n9k*0'
+#SECRET_KEY = 'django-insecure-cczrbxx)7unpkuc3^vu4e=-)dkmt3^d^m8q=1#nb!g7n5n9k*0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
